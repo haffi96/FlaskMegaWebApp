b0541ed56c97593a8a26b2cf19937e6e2fbe5924
Haffi Mazhar <haffimazhar96@gmail.com>
2019-11-22 23:50:52 +0000

    Avatar fixed

--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,7 @@ from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 from werkzeug.urls import url_parse
 
+
 @app.route('/')
 @app.route('/index')
 @login_required
@@ -12,15 +13,16 @@ def index():
     user = {'username': 'Miguel'}
     posts = [
         {
-            'author':{'username':'John'},
-            'body':'Beautiful day in Portland!'
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
         },
         {
-            'author':{'username':'Susan'},
+            'author': {'username': 'Susan'},
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html',title='Home Page', posts=posts)
+    return render_template('index.html', title='Home Page', posts=posts)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -33,17 +35,19 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        next_page =request.args.get('next')
+        next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
             return redirect(next_page)
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
 
+
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -58,3 +62,14 @@ def register():
         flash('Congragulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post#1'},
+        {'author': user, 'body': 'Test post#2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
